2f5e0963b9b25b7a12d4a683178b195bc8c263b1
Victor Ferreira <160291139+victorferreiradev@users.noreply.github.com>
2026-02-12 09:34:53 -0300

    Update last update date to February 12, 2026

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Data limite em que os dados estão consolidados
 // IMPORTANTE: deve representar a ÚLTIMA DATA COMPLETA disponível
-const DATA_ULTIMA_ATUALIZACAO = "2026-02-09";
+const DATA_ULTIMA_ATUALIZACAO = "2026-02-12";
 
 // Exibe dinamicamente a última data de extração no HTML
 document.addEventListener("DOMContentLoaded", () => {
